2fb1921038d8737b667536de45edb8dd6a7670c3
System Administrator <root@Bens-MBP.lan>
2022-06-18 16:45:48 +0100

    test

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,33 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "reactamplify8fd202a3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "todo": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "analytics": {
+        "reactamplify": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    },
+    "storage": {
+        "s3f92b97c4": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react-v1";
+import { API, graphqlOperation, Analytics, Auth, Storage } from "aws-amplify";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const listTodos = `query listTodos {
+  listTodos{
+    items{
+      id
+      name
+      description
+    }
+  }
+}`;
+
+const addTodo = `mutation createTodo($name:String! $description: String!) {
+  createTodo(input:{
+    name:$name
+    description:$description
+  }){
+    id
+    name
+    description
+  }
+}`;
+
+class App extends Component {
+
+  uploadFile = (evt) => {
+    const file = evt.target.files[0];
+    const name = file.name;
+    Storage.put(name, file).then(() => {
+      this.setState({ file: name });
+    })
+  }
+
+  state = { username: "" };
+  async componentDidMount() {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      this.setState({ username: user.username });
+    } catch (err) {
+      console.log("error getting user: ", err);
+    }
+  }
+  recordEvent = () => {
+    Analytics.record({
+      name: "My test event",
+      attributes: {
+        username: this.state.username
+      }
+    });
+  };
+
+
+  todoMutation = async () => {
+    const todoDetails = {
+      name: "Party tonight!",
+      description: "Amplify CLI rocks!"
+    };
+
+    const newTodo = 
+    await API.graphql({
+      query: addTodo,
+      variables: todoDetails,
+      authMode: 'API_KEY'
+    });
+    alert(JSON.stringify(newTodo));
+  };
+
+  listQuery = async () => {
+    console.log("listing todos");
+    const allTodos = 
+    await API.graphql({
+      query: listTodos,
+      authMode: 'API_KEY'
+    });
+    alert(JSON.stringify(allTodos));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <AmplifySignOut />
+        <p> Click a button </p>
+        <button onClick={this.listQuery}>GraphQL List Query</button>
+        <button onClick={this.todoMutation}>GraphQL Todo Mutation</button>
+        <button onClick={this.recordEvent}>Record Event</button>
+        <p> Pick a file</p>
+        <input type="file" onChange={this.uploadFile} />  
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, true);
